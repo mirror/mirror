7c29cfebdb5ad020b92a71cd1687f4bcdc441785
John Sebastian Peterson <jpeterson57@gmail.com>
2024-09-25 03:24:53 +0300

    add --interactive

--- git_mirror.sh
+++ git_mirror.sh
@@ -6,6 +6,7 @@ function show_usage() {
 	echo mirror repo to git hub
 	echo 'example usage: git_mirror.sh abc bzr http://launchpad/abc "" --debug --dry-run
 --fast skip svn cvs
+--interactive require user input
 --resume experiment to resume existing svn cvs mirror'
 	exit 0
 }
@@ -24,6 +25,7 @@ issvn=false
 isdebug=false
 isdry=false
 isfast=false
+isint=false
 isresume=false
 istest=false
 
@@ -37,12 +39,78 @@ case "$arg" in
 		isdry=true;;
 	-f|--fast)
 		isfast=true;;
+	-i|--interactive)
+		isint=true;;
 	-r|--resume)
 		isresume=true;;
 	-t|--test)
 		istest=true;;
 esac
 done
+
+# disable all commands
+if $isdry; then
+	shopt -s expand_aliases
+	for a in bzr git hg mkdir popd pushd rm svn; do
+		alias $a="echo $a"
+	done
+	# type git; git -v
+fi
+
+function format() {
+	if $1; then
+		bold=$(tput bold)
+	fi
+	if [ $2 -gt -1 ]; then
+		color=$(tput setaf $2)
+	fi
+	normal=$(tput sgr0)
+	shift 2
+	echo "${bold}${color}$@${normal}"
+}
+
+function is_big() {
+	echo checking size
+	json=$(curl -s https://api.github.com/repos/$org/$to)
+	size=$(echo $json|jshon -e size -u)
+if [ $size -gt 100000 ]; then
+	format false 9 $(numfmt --to=iec $(($size*1024))) too big for --fast
+	exit 1
+fi
+}
+
+# are we behind ?
+function is_behind() {
+	echo comparing head dates
+
+	case "$type" in
+	git)
+		local c="clone --quiet --depth=1";;
+	hg)
+		local c=" hg clone";;
+	*) format false 9 add $type date comparison
+		return 0;;
+	esac
+
+	# shopt -u expand_aliases
+	rm -rf tmp
+	mkdir tmp
+	pushd tmp
+	git $c $from .
+	sd=$(TZ=UTC0 git show HEAD --quiet --date=iso-strict-local --format="%cd"|sed "s,+00:00,Z,")
+	popd
+	# shopt -s expand_aliases
+	
+	json=$(curl -s https://api.github.com/repos/$org/$to/branches/master)
+	md=$(echo $json|jq -r '.. | .date? | strings'|tail -1)
+
+	if [ "$sd" == "$md" ]; then
+		echo $to up to date
+		exit 0
+	else
+		echo head dates differ $sd $md
+	fi
+}
 
 function mirror_exist() {
 	if $isdry; then
@@ -51,7 +119,6 @@ function mirror_exist() {
 	fi
 	echo check for existing repo
 	json=$(curl -s https://api.github.com/repos/$org/$to)
-	# echo $json
 	error=$(echo "$json" | jshon -Q -e message -u)
 	if [ -n "$error" ] && [[ "$error" != "API rate limit exceeded"* ]]; then
 		echo $error;
@@ -91,14 +158,21 @@ function create_repo() {
 	from=$3
 	arg=$4
 
+	# title
+	echo 
+	format true -1 $to
+
 	# repo home
-	if [ -z "$REPO_HOME" ]; then echo "REPO_HOME not set using ."; REPO_HOME=.; fi
+	if [ -z "$REPO_HOME" ]; then
+		# echo "REPO_HOME not set using ."
+		REPO_HOME=.
+	fi
 	if [ ! -d "$REPO_HOME" ]; then echo "$REPO_HOME doesn't exist"; return 1; fi
 	dir=$REPO_HOME/$to
 
 # target org
 if [ -z "$org" ]; then
-	echo org is unset using mirror
+	# echo org is unset using mirror
 	org=mirror
 fi
 
@@ -107,23 +181,12 @@ if $isdebug; then
 	set -x
 fi
 
-# disable all commands
-if $isdry; then
-	shopt -s expand_aliases
-	for a in bzr git hg mkdir popd pushd rm svn; do
-		alias $a="echo $a"
-	done
-	# type git; git -v
-fi
-
 # test
 if $istest; then
-	mirror_exist
+	is_behind
+	# mirror_exist
 	exit
 fi
-
-	# log
-	echo pushing $to $(date -Is)
 	
 	# type
 	m="push --mirror"
@@ -156,17 +219,17 @@ fi
 		m="push origin HEAD:$branch"
 	fi
 
-
 # clone
 # why do we need this?
 # isrepo=($isgit && -f $dir/HEAD || !$isgit && -f $dir/.git/HEAD)
 if [[ -d $dir ]]; then
 	echo resume existing repo
 	pushd $dir
+	git $p
 
 else
 	if $isfast && ($issvn || $iscvs); then
-		echo --fast disable $type
+		echo --fast disable new clones for $type
 		exit 1
 	fi
 
@@ -174,6 +237,10 @@ else
 		echo $type not support resume
 		exit 1
 	fi
+
+	is_big
+
+	is_behind
 
 	echo creating new folder $to
 	mkdir -p $dir
@@ -203,22 +270,26 @@ else
 	fi
 fi
 
-	# select branch
+	# select branch - fix this
 	if $isbzr; then
 		git checkout bzr/master
 	fi
 
 	# sync
-	git $p
 	git $m
 res=$?
 
 	# return to home
 	popd
 
+# keep big and slow repos 
 if [ $isfast -a $res -eq 0 ]; then
-	echo fast success $to
+	format false 10 fast success $to
 	rm -rf $dir
+fi
+
+if $isint; then
+	read -p "press return to continue"
 fi
 
 if $isdebug; then set +x; fi
